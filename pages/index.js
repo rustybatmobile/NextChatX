7b77924be2be65ef7860d089c83d96ef89da78bd
Harish Neel <contact@harishcode.com>
2023-05-19 21:27:21 +0530

    Sign in form designed and all the context methods provided into the index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "../context";
+
+import {useRouter} from 'next/router';
+
+import axios from 'axios';
+
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+
+  const {
+    username, 
+    setUsername, 
+    secret, 
+    setSecret
+  } = useContext(Context)
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={e => e.preventDefault()}>
+          <div className="auth-title">NextJS Chat</div>
+
+          <div className="input-container">
+            <input 
+              placeholder="Email"
+              className="text-input"
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <input 
+              type = "password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </div>
+          <button 
+            type = "submit"
+            className="submit-button"
+          >Login / Sign up</button>
+        </form>
+      </div>
+    </div>
+  )
 }
